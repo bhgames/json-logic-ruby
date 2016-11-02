0f572bc628af8a8b1df05084ddcacaeaf916eb92
Kenneth Geerts <kenneth.geerts@up-nxt.com>
2016-11-02 21:47:06 +0100

    Fix bug with wrapped arrays

--- lib/json_logic/rule_set.rb
+++ lib/json_logic/rule_set.rb
@@ -7,9 +7,10 @@ module JSONLogic
 
     def evaluate(logic = @logic)
       return logic unless logic.is_a?(Hash)
-      operator, values = logic.first # unwrap 1-key hash
+      operator, values = logic.first # unwrap hash with single key
       values = [values] unless values.is_a? Array
       evaluated_values = values.map { |value| evaluate(value) }
+      evaluated_values.flatten!(1) if evaluated_values.size == 1 # [['A']] => ['A']
       Operation.perform(operator, evaluated_values, @data)
     end
   end
